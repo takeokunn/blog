c4df521579bb0c3065295e0234adeea3a0fdb87a
takeokunn <bararararatty@gmail.com>
2023-01-17 17:14:41 +0900

    Update

--- script.el
+++ script.el
@@ -4,8 +4,10 @@
 (package-refresh-contents)
 (package-initialize)
 (package-install 'ox-hugo)
+(package-install 'org-roam)
 
 (require 'ox-hugo)
+(require 'org-roam)
 
 (defun export-org-files ()
   "Re-exports all Org-roam files to Hugo markdown."
@@ -15,3 +17,20 @@
                        (file-expand-wildcards "org/**/*.org")))
       (with-current-buffer (find-file f)
         (org-hugo-export-wim-to-md)))))
+
+(defun collect-backlinks-string (backend)
+  (when (org-roam-node-at-point)
+    (goto-char (point-max))
+    ;; Add a new header for the references
+    (let* ((backlinks (org-roam-backlinks-get (org-roam-node-at-point))))
+      (when (> (length backlinks) 0)
+        (insert "\n\n* Backlinks\n"))
+      (dolist (backlink backlinks)
+        (let* ((source-node (org-roam-backlink-source-node backlink))
+               (node-file (org-roam-node-file source-node))
+               (file-name (file-name-nondirectory node-file))
+               (title (org-roam-node-title source-node)))
+          (insert
+           (format "- [[./%s][%s]]\n" file-name title)))))))
+
+(add-hook 'org-export-before-processing-functions #'collect-backlinks-string)
