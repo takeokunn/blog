ab52bc3e0571ff0f27343e429c5cf78aa4084ef7
takeokunn <bararararatty@gmail.com>
2025-01-19 16:24:07 +0900

    Update

---
+++ typst/flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        nativeBuildInputs = with pkgs; [
+          typst
+          migu
+          (emacs.pkgs.withPackages (epkgs: with epkgs; [ org ox-typst ]))
+        ];
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [ typst ];
+          };
+
+          packages = {
+            phperkaigi-2025-pamphlet = pkgs.stdenv.mkDerivation {
+              name = "phperkaigi-2025-pamphlet";
+              src = ./phperkaigi-2025-pamphlet;
+              nativeBuildInputs = nativeBuildInputs;
+              buildPhase = ''
+                emacs --batch \
+                      --eval "(progn
+                                (require 'ox-typst)
+                                (find-file \"article.org\")
+                                (org-typst-export-to-typst))"
+
+                export TYPST_FONT_PATHS="${pkgs.migu}/share/fonts/truetype/migu"
+                typst compile article.typ
+              '';
+              installPhase = ''
+                mkdir -p $out
+                cp article.pdf $out/phperkaigi-2025-pamphlet.pdf
+              '';
+            };
+          };
+        }
+    );
+}
