29a8841bd68e9bc92b9d874fac973dfb95662e3a
takeokunn <bararararatty@gmail.com>
2025-01-25 15:01:41 +0900

    update

--- typst/flake.nix
+++ typst/flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    typst-packages = {
+      url = "github:typst/packages";
+      flake = false;
+    };
   };
 
   outputs =
@@ -9,16 +13,25 @@
       self,
       nixpkgs,
       flake-utils,
+      typst-packages
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        nativeBuildInputs = with pkgs; [
-          typst
-          migu
-          (emacs.pkgs.withPackages (epkgs: with epkgs; [ org ox-typst ]))
-        ];
+        typstPackagesSrc = pkgs.symlinkJoin {
+          name = "typst-packages-src";
+          paths = [ "${typst-packages}/packages" ];
+        };
+        typstPackagesCache = pkgs.stdenvNoCC.mkDerivation {
+          name = "typst-packages-cache";
+          src = typstPackagesSrc;
+          dontBuild = true;
+          installPhase = ''
+            mkdir -p $out/typst/packages
+            cp -LR --reflink=auto --no-preserve=mode -t $out/typst/packages $src/*
+          '';
+        };
       in
         {
           devShells.default = pkgs.mkShell {
@@ -29,7 +42,11 @@
             phperkaigi-2025-pamphlet = pkgs.stdenv.mkDerivation {
               name = "phperkaigi-2025-pamphlet";
               src = ./phperkaigi-2025-pamphlet;
-              nativeBuildInputs = nativeBuildInputs;
+              nativeBuildInputs = with pkgs; [
+                typst
+                migu
+                (emacs.pkgs.withPackages (epkgs: with epkgs; [ org ox-typst ]))
+              ];
               buildPhase = ''
                 emacs --batch \
                       --eval "(progn
@@ -39,12 +56,12 @@
                                 (org-typst-export-to-typst))"
 
                 export TYPST_FONT_PATHS="${pkgs.migu}/share/fonts/truetype/migu"
+                export TYPST_PACKAGE_PATH="${typstPackagesCache}/typst/packages"
                 typst compile article.typ
               '';
               installPhase = ''
                 mkdir -p $out
                 cp article.pdf $out/phperkaigi-2025-pamphlet.pdf
-                cp article.typ $out/phperkaigi-2025-pamphlet.typ
               '';
             };
           };
