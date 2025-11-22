87fbeb432ceb6cd3313a6e5955e546ff4a16614e
takeokunn <bararararatty@gmail.com>
2025-11-22 22:09:59 +0900

    feat(devto): add custom dev.to org-export backend and template
    - Implement org-devto backend for org-export with YAML front matter
    - Add dev.to-specific template and code block formatting
    - Update publishing function in export-org-devto-files
    - Fix metadata keys in sample article for dev.to compatibility
    - Remove dry_run from GitHub workflow for production publishing

--- devto/scripts/ox-devto.el
+++ devto/scripts/ox-devto.el
@@ -2,8 +2,49 @@
 
 (setq org-publish-timestamp-directory "./.org-timestamps")
 
-(setq org-jekyll-md-layout nil)
-(setq org-jekyll-md-include-yaml-front-matter t)
+(defun org-devto--yaml-front-matter (info)
+  "dev.to 用の YAML front matter を生成"
+  (let ((title (org-export-data (plist-get info :title) info))
+        (published (plist-get info :devto-published))
+        (tags (plist-get info :devto-tags))
+        (description (plist-get info :description)))
+    (concat "---\n"
+            (format "title: \"%s\"\n" title)
+            (format "published: %s\n" (or published "false"))
+            (when tags (format "tags: %s\n" tags))
+            (when description (format "description: %s\n" description))
+            "---\n\n")))
+
+(defun org-devto-template (contents info)
+  "dev.to 用のテンプレート関数"
+  (concat (org-devto--yaml-front-matter info)
+          contents))
+
+(defun org-devto-src-block (src-block _contents info)
+  "GFM 形式のコードブロックを生成（Jekyll の highlight 形式ではなく）"
+  (let ((lang (org-element-property :language src-block))
+        (code (org-export-format-code-default src-block info)))
+    (format "```%s\n%s```\n" (or lang "") code)))
+
+;; ox-jekyll-md を継承して devto バックエンドを定義
+(org-export-define-derived-backend 'devto 'jekyll
+  :options-alist
+  '((:devto-published "PUBLISHED" nil "false" t)
+    (:devto-tags "TAGS" nil nil t)
+    (:description "DESCRIPTION" nil nil t))
+  :translate-alist
+  '((template . org-devto-template)
+    (src-block . org-devto-src-block)))
+
+(defun org-devto-export-to-md (&optional async subtreep visible-only)
+  "現在のバッファを dev.to 用 markdown ファイルにエクスポート"
+  (interactive)
+  (let ((outfile (org-export-output-file-name ".md" subtreep)))
+    (org-export-to-file 'devto outfile async subtreep visible-only)))
+
+(defun org-devto-publish-to-md (plist filename pub-dir)
+  "org ファイルを dev.to 用 markdown にパブリッシュ"
+  (org-publish-org-to 'devto filename ".md" plist pub-dir))
 
 (defun export-org-devto-files ()
   (let ((org-publish-project-alist `(("devto"
@@ -11,5 +52,5 @@
                                       :base-directory "articles"
                                       :base-extension "org"
                                       :publishing-directory "public/"
-                                      :publishing-function org-jekyll-md-publish-to-md))))
+                                      :publishing-function org-devto-publish-to-md))))
     (org-publish-all t)))
