4cd7d5ed1dfcfebe9d98d9f19c74c2a38d310553
takeokunn <takeokunn@users.noreply.github.com>
2026-07-04 20:45:40 +0000

    deploy: d3db76e33e7a383f5c80b60239bcdaf0de72b9d9

---
+++ graph/assets/index-CtISg59_.js
@@ -0,0 +1,52 @@
+var i=/\s*\n\s*/g,a=e=>e.replaceAll(i,"").trim();function s(e={visibility:"hover",feedbackDuration:3e3}){return{name:"@rehype-pretty/transformers/copy-button",code(t){t.children.push({type:"element",tagName:"button",properties:{type:"button",title:"Copy code","aria-label":"Copy code",data:this.source,class:"rehype-pretty-copy",onclick:a(`
+            navigator.clipboard.writeText(this.attributes.data.value);
+            this.classList.add('rehype-pretty-copied');
+            window.setTimeout(() => this.classList.remove('rehype-pretty-copied'), ${e.feedbackDuration});
+          `)},children:[{type:"element",tagName:"span",properties:{class:"ready"},children:[]},{type:"element",tagName:"span",properties:{class:"success"},children:[]}]}),t.children.push({type:"element",tagName:"style",properties:{},children:[{type:"text",value:o({copyIcon:e.copyIcon,successIcon:e.successIcon,visibility:e.visibility})}]})}}}function o({copyIcon:e="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 48 48'%3E%3Cpath fill='%23adadad' d='M16.187 9.5H12.25a1.75 1.75 0 0 0-1.75 1.75v28.5c0 .967.784 1.75 1.75 1.75h23.5a1.75 1.75 0 0 0 1.75-1.75v-28.5a1.75 1.75 0 0 0-1.75-1.75h-3.937a4.25 4.25 0 0 1-4.063 3h-7.5a4.25 4.25 0 0 1-4.063-3M31.813 7h3.937A4.25 4.25 0 0 1 40 11.25v28.5A4.25 4.25 0 0 1 35.75 44h-23.5A4.25 4.25 0 0 1 8 39.75v-28.5A4.25 4.25 0 0 1 12.25 7h3.937a4.25 4.25 0 0 1 4.063-3h7.5a4.25 4.25 0 0 1 4.063 3M18.5 8.25c0 .966.784 1.75 1.75 1.75h7.5a1.75 1.75 0 1 0 0-3.5h-7.5a1.75 1.75 0 0 0-1.75 1.75'/%3E%3C/svg%3E",successIcon:t="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='%2366ff85' d='M9 16.17L5.53 12.7a.996.996 0 1 0-1.41 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71a.996.996 0 1 0-1.41-1.41z'/%3E%3C/svg%3E",visibility:p="hover"}={}){let r=`
+    :root {
+      --copy-icon: url("${e}");
+      --success-icon: url("${t}");
+    }
+    pre:has(code) {
+      position: relative;
+    }
+    pre button.rehype-pretty-copy {
+      right: 1px;
+      padding: 0;
+      width: 24px;
+      height: 24px;
+      display: flex;
+      margin-top: 2px;
+      margin-right: 8px;
+      position: absolute;
+      border-radius: 25%;
+      backdrop-filter: blur(3px);
+      & span {
+        width: 100%;
+        aspect-ratio: 1 / 1;
+      }
+      & .ready {
+        background-image: var(--copy-icon);
+      }
+      & .success {
+        display: none; background-image: var(--success-icon);
+      }
+    }
+    &.rehype-pretty-copied { 
+      & .success { 
+        display: block;
+      } & .ready {
+        display: none;
+      }
+    }
+    pre button.rehype-pretty-copy.rehype-pretty-copied {
+      opacity: 1;
+      & .ready { display: none; }
+      & .success { display: block; }
+    }
+`;return p==="hover"&&(r+=`
+        pre button.rehype-pretty-copy { opacity: 0; }
+        figure[data-rehype-pretty-code-figure]:hover > pre > code button.rehype-pretty-copy {
+          opacity: 1;
+        }
+      `),a(r)}export{s as transformerCopyButton};
---
+++ graph/assets/isSymbol-CPMUUYKH.js
@@ -0,0 +1,1 @@
+import{i as b,b as s}from"./isObjectLike-DZOcISe9.js";var t="[object Symbol]";function e(o){return typeof o=="symbol"||b(o)&&s(o)==t}export{e as i};
